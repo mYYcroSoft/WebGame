f3b55e80aa04d43f961dc3f04352a2debd84c4d6
mYcroSoft <110401497+mYYcroSoft@users.noreply.github.com>
2023-10-05 11:46:03 +0200

    Generate mesh
    jquery append, styling mesh in progress

--- theGame/index.js
+++ theGame/index.js
@@ -1,11 +1,53 @@
-let player_pos = [/*x , y*/ 0,0]
-let platformY = document.getElementById('platform').style.tops
+const scene = document.getElementById('screen')
 
-let  Objects = [
-    'player' =  {
 
+let materials = {
+    'dirt': './materials/dirt.jpg', 
+}
+let objects = {
+    player: {
+        pos: [15, 15],
+        data: {},
+        type: 'controller',
+        canColide: false,
+        pos: {x: 25, y: 25},
+        visual: {
+            width: 25,
+            height: 25,
+            text_color: '#000',
+            color: '#fff',
+            material: 'dirt',
+        }
+    },
+ 
+};
+
+
+function redner_object(object){
+    const obj = objects[object]
+    let visualData = obj.visual
+    $('#screen').append(
+        `
+        <canvas id="${object}" width="${visualData.width}" height="${visualData.height}"></canvas>
+        `
+    )
+    let renObj = document.getElementById(object)
+    for (let property in visualData) {
+        if(property == 'color'){
+            renObj.style.backgroundColor = visualData[property];
+        } else if (property == 'material') {
+            let materialUrl = materials[property]
+       /* renObj.style.backgroundImage = url('./materials/dirt.jpg');*/
+           
+        } else {
+            renObj.style[property] = visualData[property];
+        }
+     
     }
-]
+
+}
+
+redner_object('player')
 
 document.addEventListener('keydown', function(event) {
     if(event.key == 'd'){
